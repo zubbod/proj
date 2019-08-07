d28e8702343d9070c9e8f61b817e4d51c9e85ecc
Zb <zubbod@gmail.com>
2019-08-07 22:17:08 +0300

    создание формы

--- proj/src/app/my/my.component.ts
+++ proj/src/app/my/my.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormControlName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup = null;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      sum: ['', Validators.required]
+    });
   }
 
+  ngOnInit() {
+    console.log(this.form);
+    this.setValidators(this.form);
+  }
+
+  private setValidators(form: FormGroup) {
+    form.controls.sum.setValidators([Validators.required]);
+  }
+
+  get sum() {
+    return this.form.get('sum');
+  }
 }
