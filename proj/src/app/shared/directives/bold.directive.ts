272a992384c11dd31555391ccfaf4cae1457564c
Zubashenko Bogdan <b.zubashenko@solarl.ru>
2019-08-07 13:47:24 +0300

    доработка директивы

--- proj/src/app/shared/directives/bold.directive.ts
+++ proj/src/app/shared/directives/bold.directive.ts
@@ -1,12 +1,48 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appBold]'
 })
-export class BoldDirective {
+export class BoldDirective implements OnInit {
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'font-weight', 'bold');
-   }
+  @Input() selectedSize = '1.1rem';
+  @Input() defaultSize = '1rem';
+
+  private fontSize: string;
+  private fontWeight = 'normal';
+  private marginBottom: string;
+  private defaultMarginBottom = '0.2rem';
+
+  ngOnInit() {
+    this.fontSize = this.defaultSize;
+    this.marginBottom = '0.2rem';
+  }
+
+  @HostBinding('style.fontSize') get getFontSize() {
+    return this.fontSize;
+  }
+
+  @HostBinding('style.marginBottom') get getMarginBottom() {
+    return this.marginBottom;
+  }
+
+  @HostBinding('style.fontWeight') get getFontWeight() {
+    return this.fontWeight;
+  }
+
+  @HostBinding('style.cursor') get getCursor() {
+    return 'pointer';
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.fontWeight = 'bold';
+    this.fontSize = this.selectedSize;
+    this.marginBottom = `${parseFloat(this.defaultMarginBottom) - (parseFloat(this.defaultSize) - parseFloat(this.selectedSize))}rem`;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.fontWeight = 'normal';
+    this.fontSize = this.defaultSize;
+    this.marginBottom = this.defaultMarginBottom;
+  }
 
 }
